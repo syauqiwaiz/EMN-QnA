09f66e3715117fbba1e31dec0426e2c42db6dcd2
Syauqi Waiz <88612584+syauqiwaiz@users.noreply.github.com>
2021-10-23 10:53:53 +0800

    Update main.py

--- source-code/main.py
+++ source-code/main.py
@@ -8,4 +8,10 @@ from torch import optim
 from model import MemN2N
 from helpers import dataloader, get_fname, get_params
 
- 
+def train(train_iter, model, optimizer, epochs, max_clip, valid_iter=None):
+   total_loss = 0
+   valid_data = list(valid_iter)
+   valid_loss = None
+   next_epoch_to_report = 5
+   pad = model.vocab.stoi['<pad>']
+   
