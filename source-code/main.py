3a1586f2f0a3b195f9a263a2313d7a5791c5ff52
Syauqi Waiz <88612584+syauqiwaiz@users.noreply.github.com>
2021-10-23 10:55:45 +0800

    Update main.py

--- source-code/main.py
+++ source-code/main.py
@@ -15,3 +15,15 @@ def train(train_iter, model, optimizer, epochs, max_clip, valid_iter=None):
    next_epoch_to_report = 5
    pad = model.vocab.stoi['<pad>']
    
+   for _, batch in enumerate(train_iter, start=1):
+      story = batch.story
+      query = batch.query
+      answer = batch.answer
+
+      optimizer.zero_grad()
+      outputs = model(story.cuda(), query.cuda())
+      loss = F.nll_loss(outputs, answer.view(-1).cuda(), ignore_index=pad, reduction='sum')
+      loss.backward()
+      nn.utils.clip_grad_norm_(model.parameters(), max_clip)
+      optimizer.step()
+      total_loss += loss.item()
