f3febf11d37b7b959776a0a0374595f40b26410a
Syauqi Waiz <88612584+syauqiwaiz@users.noreply.github.com>
2021-10-23 10:57:28 +0800

    Update main.py

--- source-code/main.py
+++ source-code/main.py
@@ -27,3 +27,18 @@ def train(train_iter, model, optimizer, epochs, max_clip, valid_iter=None):
       nn.utils.clip_grad_norm_(model.parameters(), max_clip)
       optimizer.step()
       total_loss += loss.item()
+      
+      # linear start
+     if model.use_ls:
+         loss = 0
+         for k, batch in enumerate(valid_data, start=1):
+             story = batch.story
+             query = batch.query
+             answer = batch.answer
+             outputs = model(story.cuda(), query.cuda())
+             loss += F.nll_loss(outputs, answer.view(-1).cuda(), ignore_index=pad, reduction='sum').item()
+         loss = loss / k
+         if valid_loss and valid_loss <= loss:
+                model.use_ls = False
+         else:
+                valid_loss = loss
