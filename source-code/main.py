71d5188a9e75c7a50be6b64ab170a669783d5c03
Syauqi Waiz <88612584+syauqiwaiz@users.noreply.github.com>
2021-10-23 10:53:10 +0800

    Update main.py

--- source-code/main.py
+++ source-code/main.py
@@ -1,1 +1,11 @@
+#EMN implementation to solve BABI tasks based on https://github.com/zshihang/MemN2N
 
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from model import MemN2N
+from helpers import dataloader, get_fname, get_params
+
+ 
